c69c839d60d2c3849b090a92a5b947d85906e998
Li Xiao <M42M42M42@163.com>
2026-06-24 20:36:41 +0800

    feat: add cloud-provider module

---
+++ cloud-demo/cloud-provider/src/main/java/org/example/ProviderApplication.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @GetMapping("/provider/hello")
+    public String hello() {
+        return "Hello from Provider! (Port: 8081)";
+    }
+}
