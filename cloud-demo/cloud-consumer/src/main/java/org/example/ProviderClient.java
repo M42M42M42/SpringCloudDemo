9f526b1a851717501d3709325a23ce31cb963126
Li Xiao <M42M42M42@163.com>
2026-06-25 04:54:29 +0800

    feat: use Sentinel and config fallback for Provider

--- cloud-demo/cloud-consumer/src/main/java/org/example/ProviderClient.java
+++ cloud-demo/cloud-consumer/src/main/java/org/example/ProviderClient.java
@@ -1,10 +1,19 @@
 package org.example;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "cloud-provider")
+@FeignClient(name = "cloud-provider", fallback = ProviderClientFallBack.class)
 public interface ProviderClient {
     @GetMapping("/provider/hello")
     String getValue();
+}
+
+@Component
+class ProviderClientFallBack implements ProviderClient {
+    @Override
+    public String getValue() {
+        return "System is busy or Provider is dead! This is Sentinel fallback.";
+    }
 }
